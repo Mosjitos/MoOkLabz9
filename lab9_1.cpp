9fbe81b5a37730135d96b86786c07da84e439374
Mosjitos <kamolrat_saeboon@cmu.ac.th>
2023-12-16 18:27:20 +0700

    lab9_1.cpp Completed after I gave up ><

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+    double initL, rate, pay;
 	cout << "Enter initial loan: ";
+    cin >> initL;
 	cout << "Enter interest rate per year (%): ";
+    cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+    cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,48 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
+
+	//first round add way too many input, so I set first round saparately. In order to print out payment and NewBalance, I need to check the condition before print out.
+	//I need to do it, Dahhh!!.
+    double prevB=initL,  interest, total, newB;
+
+    cout << fixed << setprecision(2); 
 	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << prevB;
+        interest = prevB*(rate/100);
+	cout << setw(13) << left << interest;
+        total = prevB + interest;
+	cout << setw(13) << left << total;
+	if(pay < total){
+            cout << setw(13) << left << pay;
+    }else{cout << setw(13) << left << total;}
+        newB = total - pay;
+    if(newB > 0){
+        cout << setw(13) << left << newB;
+    }else{cout << setw(13) << left << 0.00;}
 	cout << "\n";	
+    
+    int i=2;
+	//in do-while I added break in sub if-else, because I wanted to end da loop.
+    do{
+    if(prevB > pay){
+        cout << setw(13) << left << i; 
+        cout << setw(13) << left << newB;
+            interest = newB*(rate/100);
+	    cout << setw(13) << left << interest;
+            total = newB + interest;
+        cout << setw(13) << left << total;
+        if(pay < total){
+            cout << setw(13) << left << pay;
+        }else{cout << setw(13) << left << total;}
+        newB = total - pay;
+        if(newB > 0){
+            cout << setw(13) << left << newB;
+        }else{cout << setw(13) << left << 0.00; break;}
+        cout << "\n";
+        ++i;
+    } else break;
+    } while (true);
 	
 	return 0;
 }
